09d1c6f46c8b83b5221d3717fcae2ed01a07f234
Guidozijef <913497146@qq.com>
2021-02-03 17:18:23 +0800

    配置文件

--- vue.config.js
+++ vue.config.js
@@ -33,6 +33,7 @@ module.exports = {
   // lintOnSave: false,
   // productionSourceMap: false,
   devServer: {
+    outputDir:'docs',
     historyApiFallback: true,
     https: false,
     hot: true,
