2bc611d6c90b462b8ebdfc046300155822b4eb23
Guidozijef <913497146@qq.com>
2021-02-03 17:45:03 +0800

    自动部署

--- vue.config.js
+++ vue.config.js
@@ -32,6 +32,7 @@ module.exports = {
   // assetsDir: 'static-new',
   // lintOnSave: false,
   // productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/resume-vue" : "/",
   outputDir:'docs',
   devServer: {
     historyApiFallback: true,
